e946137d54f1b6e4abbabc6c622abf96b2701537
mnj190 <mnj190@naver.com>
2026-08-17 20:16:58 +0900

    chore: configure database profiles

--- src/test/java/com/mnj190/aitrading/AiTradingPlatformApplicationTests.java
+++ src/test/java/com/mnj190/aitrading/AiTradingPlatformApplicationTests.java
@@ -2,7 +2,9 @@ package com.mnj190.aitrading;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("local")
 @SpringBootTest
 class AiTradingPlatformApplicationTests {
 
