a0687744f4037c3b32a78ea841452660711e6869
mnj190 <mnj190@naver.com>
2026-08-17 19:39:23 +0900

    first commit

---
+++ src/main/java/com/mnj190/aitrading/AiTradingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.mnj190.aitrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTradingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiTradingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mnj190/aitrading/AiTradingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnj190.aitrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTradingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
